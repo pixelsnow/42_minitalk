0f9020c15d6fefb73bc5818c14d389897d074680
Valeria Vagapova <lera.vagapova@gmail.com>
2023-05-20 22:52:46 +0300

    add process id check

--- client.c
+++ client.c
@@ -6,36 +6,44 @@
 /*   By: vvagapov <vvagapov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/06 21:56:16 by vvagapov          #+#    #+#             */
-/*   Updated: 2023/05/20 22:25:47 by vvagapov         ###   ########.fr       */
+/*   Updated: 2023/05/20 22:52:23 by vvagapov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static void sig_handler(int signum)
+static void signal_handler(int signum)
 {
-	ft_printf("confirmation %i\n", signum);
+	if (signum)
+		ft_printf("Message received!\n");
+}
+
+int input_invalid(int ac, char** av)
+{
+	if (ac != 3 || !av || !av[0] || !av[1] || av[2])
+		return (1);
+	return (0);
 }
 
 int main(int ac, char** av)
 {
-	int		pid;
-	int		serverPid;
+	int		server_pid;
 	char*	message;
 
-	if (ac != 3)
+	if (input_invalid(ac, av))
 	{
 		ft_printf("Please provide server process id and your message\n");
 		return (1);
 	}
-		
-	pid = getpid();
-	ft_printf("Client process id is: %i\n", pid);
-
-	serverPid = ft_atoi(av[1]);
+	server_pid = ft_atoi(av[1]);
+	if (kill(server_pid, 0))
+	{
+		ft_printf("Process with this id doesn't exist\n");
+		return (1);
+	}
 	message = av[2];
-	kill(serverPid, SIGUSR1);
-	signal(SIGUSR1, sig_handler);
+	kill(server_pid, SIGUSR1);
+	signal(SIGUSR1, signal_handler);
 	pause();
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,20 +6,15 @@
 /*   By: vvagapov <vvagapov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/06 22:18:18 by vvagapov          #+#    #+#             */
-/*   Updated: 2023/05/20 22:26:46 by vvagapov         ###   ########.fr       */
+/*   Updated: 2023/05/20 22:38:12 by vvagapov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-/* static void sig_handler(int signum)
+static void    signal_handler(int signo, siginfo_t *info, void *context)
 {
-	ft_printf("got your message %i\n", signum);
-} */
-
-void    signal_handler(int signo, siginfo_t *info, void *context)
-{
-	ft_printf("signo %i\n", signo);
+	ft_printf("Received signo %i\n", signo);
 	(void)context;
 	kill(info->si_pid, SIGUSR1);
 }
@@ -29,10 +24,8 @@ int	main(void)
 	int 				pid;
 	struct sigaction	action;
 
-	ft_printf("hello\n");
 	pid = getpid();
 	ft_printf("Server process id: %i\n", pid);
-	//signal(SIGUSR2, sig_handler);
 	sigemptyset(&action.sa_mask); // this init is required before using sigset_t
 	action.sa_flags = SA_SIGINFO; // sa_sigaction is pointed at handler function
 	action.sa_sigaction = signal_handler; // setting handler
@@ -40,7 +33,7 @@ int	main(void)
  	while (1)
 	{
 		sigaction(SIGUSR1, &action, NULL);
-		ft_printf("pause\n");
+		sigaction(SIGUSR2, &action, NULL);
 		pause();
 	}
 	return (0);
