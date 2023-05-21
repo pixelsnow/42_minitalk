b1c1d8519145b39e970b9743d9b34d9e1da87561
Valeria Vagapova <lera.vagapova@gmail.com>
2023-05-21 17:30:30 +0300

    extract reading len into a separate function

--- server.c
+++ server.c
@@ -6,11 +6,19 @@
 /*   By: vvagapov <vvagapov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/06 22:18:18 by vvagapov          #+#    #+#             */
-/*   Updated: 2023/05/21 16:48:56 by vvagapov         ###   ########.fr       */
+/*   Updated: 2023/05/21 17:29:48 by vvagapov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+static int read_len(int signo, int len)
+{
+	len = len >> 1;
+	if (signo == SIGUSR2)
+		len = len | (1 << 30);
+	return len;
+}
 
 static void    signal_handler(int signo, siginfo_t *info, void *context)
 {
@@ -18,48 +26,41 @@ static void    signal_handler(int signo, siginfo_t *info, void *context)
 	static int	i = 0;
 	static char	*res;
 
-	
 	if (i < 31)
 	{
-		len = len >> 1;
-		if (signo == SIGUSR2)
-			len = len | (1 << 30);
-		i++;
+		len = read_len(signo, len);
 	}
 	else if (i == 31) {
-		ft_printf("LEN: %i\n", len);
+		//ft_printf("LEN: %i\n", len);
 		res = (char *)malloc((len + 1) * sizeof(char));
 		if (!res)
 		{
 			ft_printf("ERROR: memory not allocated\n");
 		}
 		res[len] = '\0';
-		ft_printf("EMPTY MSG: %s\n", res);
-		i++;
 	}
-	//ft_printf("checking if 31 < %i <= %i\n", i, len * 8 + 32);
 	else if (i > 31 && i <= len * 8 + 32)
 	{
 		if (signo == SIGUSR2)
 		{
-			ft_printf("1 to %ith bit of %ith char\n", (i - 32) % 8, (i - 32) / 8);
+			//ft_printf("1 to %ith bit of %ith char\n", (i - 32) % 8, (i - 32) / 8);
 			res[(i - 32) / 8] |= (1 << (7 - (i - 32) % 8));
-			ft_printf("res[%i] |= %i\n", (i - 32) / 8, res[(i - 32) / 8 - 1] | (1 << (7 - (i - 32) % 8)));
-			ft_printf("res[%i]: %i\n",(i - 32) / 8, res[(i - 32) / 8]);
+			//ft_printf("res[%i] |= %i\n", (i - 32) / 8, res[(i - 32) / 8 - 1] | (1 << (7 - (i - 32) % 8)));
+			//ft_printf("res[%i]: %i\n",(i - 32) / 8, res[(i - 32) / 8]);
 		}
 		else
 		{
-			ft_printf("0 to %ith bit of %ith char\n", (i - 32) % 8, (i - 32) / 8);
+			//ft_printf("0 to %ith bit of %ith char\n", (i - 32) % 8, (i - 32) / 8);
 		}
-		i++;
 	}
+	i++;
 	if (i == len * 8 + 32)
 	{
-		ft_printf("MSG: %s\n", res);
+		ft_printf("%s\n", res);
+		kill(info->si_pid, SIGUSR1);
 		free(res);
 		res = NULL;
 		len = 0;
-		kill(info->si_pid, SIGUSR1);
 		i = 0;
 	}
 	(void)context;
